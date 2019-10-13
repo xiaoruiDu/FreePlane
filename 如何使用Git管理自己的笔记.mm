7d3929ffc5c956d210c8ed2351e03d8648b20e22
DuXiaorui <xiaoruipython@gmail.com>
2019-10-13 16:27:07 +0800

    update

--- 如何使用Git管理自己的笔记.mm
+++ 如何使用Git管理自己的笔记.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="&#x5982;&#x4f55;&#x4f7f;&#x7528;Git&#x7ba1;&#x7406;&#x81ea;&#x5df1;&#x7684;&#x7b14;&#x8bb0;" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1570952606395"><hook NAME="MapStyle" zoom="1.771">
+<node TEXT="&#x5982;&#x4f55;&#x4f7f;&#x7528;Git&#x7ba1;&#x7406;&#x81ea;&#x5df1;&#x7684;&#x7b14;&#x8bb0;" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1570952606395"><hook NAME="MapStyle" zoom="1.948">
     <properties show_note_icons="true"/>
 
 <map_styles>
@@ -51,7 +51,7 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="3"/>
-<node TEXT="1-&#x642d;&#x5efa;Git&#x4ed3;&#x5e93;" POSITION="right" ID="ID_737199084" CREATED="1570952607650" MODIFIED="1570952677463">
+<node TEXT="1-&#x642d;&#x5efa;Git&#x4ed3;&#x5e93;" POSITION="right" ID="ID_737199084" CREATED="1570952607650" MODIFIED="1570954001271">
 <edge COLOR="#ff0000"/>
 <richcontent TYPE="NOTE">
 
@@ -65,8 +65,37 @@
     </p>
   </body>
 </html>
-
 </richcontent>
+<node TEXT="&#x521d;&#x59cb;&#x5316;&#x52a0;&#x4e0a;&#x4f20;&#x5230;git" ID="ID_705168777" CREATED="1570953926677" MODIFIED="1570954072413">
+<node TEXT="&#x9996;&#x6b21;&#x914d;&#x7f6e;&#x9700;&#x8981;&#x8fdb;&#x884c;&#x7684;&#x64cd;&#x4f5c;" ID="ID_660285457" CREATED="1570954754013" MODIFIED="1570954769311">
+<node TEXT="git clone  https://github.com/YourGithubUserName/mynotebook" ID="ID_540308312" CREATED="1570954626113" MODIFIED="1570954635590">
+<node TEXT="1-&#x5728;github&#x521b;&#x5efa;&#x4ed3;&#x5e93;&#x540e;clone&#x5230;&#x672c;&#x5730;" ID="ID_195686916" CREATED="1570954646713" MODIFIED="1570954695552"/>
+</node>
+<node TEXT="git config &#x2014;global user.name &#x201c;Your Name&#x201d;" ID="ID_813600848" CREATED="1570953784296" MODIFIED="1570953803251"/>
+<node TEXT="git config &#x2014;global user.email &#x201c;yourEmail@mail.com&#x201d;" ID="ID_663298779" CREATED="1570953804775" MODIFIED="1570953813235"/>
+</node>
+<node TEXT="git pull" ID="ID_574863394" CREATED="1570954373053" MODIFIED="1570954377806"/>
+<node TEXT="git &#x6dfb;&#x52a0;&#x5230;&#x7f13;&#x5b58;&#x533a;" ID="ID_91266361" CREATED="1570953876766" MODIFIED="1570953902257">
+<node TEXT="git add ." ID="ID_205724102" CREATED="1570953814079" MODIFIED="1570953843658">
+<node TEXT="&#x6240;&#x6709;&#x6587;&#x4ef6;&#x90fd;&#x52a0;&#x5165;" ID="ID_1916907190" CREATED="1570953950373" MODIFIED="1570953963238"/>
+</node>
+<node TEXT="git add filename.extension" ID="ID_890945689" CREATED="1570953848758" MODIFIED="1570953859049">
+<node TEXT="&#x6307;&#x5b9a;&#x6587;&#x4ef6;&#x52a0;&#x5165;" ID="ID_1327930969" CREATED="1570953964412" MODIFIED="1570953972888"/>
+</node>
+<node TEXT="git add *.html" ID="ID_1523596295" CREATED="1570953937437" MODIFIED="1570953939945">
+<node TEXT="&#x6307;&#x5b9a;&#x7c7b;&#x578b;&#x6587;&#x4ef6;&#x52a0;&#x5165;" ID="ID_121454994" CREATED="1570953974453" MODIFIED="1570953992383"/>
+</node>
+</node>
+<node TEXT="git status" ID="ID_197127525" CREATED="1570954115634" MODIFIED="1570954118607">
+<node TEXT="&#x68c0;&#x67e5;&#x6240;&#x6709;&#x5df2;&#x7ecf;&#x653e;&#x5165;/&#x4fee;&#x6539;&#x7684;&#x7f13;&#x5b58;&#x533a;&#x7684;&#x6587;&#x4ef6;" ID="ID_1232513665" CREATED="1570954120720" MODIFIED="1570954182946"/>
+</node>
+<node TEXT="* git rm &#x2014;cached filename.extension" ID="ID_938615888" CREATED="1570954217759" MODIFIED="1570954240227">
+<node TEXT="(&#x4ece;&#x6682;&#x5b58;&#x533a;&#x4e2d;&#x5220;&#x9664;&#x8be5;&#x6587;&#x4ef6;&#xff0c;&#x5e76;&#x5c06;&#x5176;&#x8bbe;&#x7f6e;&#x4e3a;&#x672a;&#x8ddf;&#x8e2a;&#x3002;)" ID="ID_1503584546" CREATED="1570954242639" MODIFIED="1570954249489"/>
+</node>
+<node TEXT="git commit -m &#x201c;Description of the commit&#x201d;" ID="ID_1146278627" CREATED="1570954254960" MODIFIED="1570954273489"/>
+<node TEXT="git push" ID="ID_1077448637" CREATED="1570954276199" MODIFIED="1570954358160"/>
+</node>
+<node TEXT="" ID="ID_488066993" CREATED="1570954047091" MODIFIED="1570954047091"/>
 </node>
 </node>
 </map>
